cecd626e0de5d3bbe5a785dfd7633828ec61617d
Riccardo Persiani <r.persiani92@gmail.com>
2019-12-23 16:02:26 +0100

    feat(structs): add another user

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -16,5 +16,11 @@ fn build_user(username: String, email: String) -> User {
 
 fn main() {
     let user1 = build_user(String::from("riccardopersiani"),String::from("test@mail.com"));
-    println!("User: {}, {}, {}, {}", user1.username, user1.email, user1.active, user1.sign_in_count);
+    println!("user1: {}, {}, {}, {}", user1.username, user1.email, user1.active, user1.sign_in_count);
+    let user2 = User {
+        email: String::from("example@mail.com"),
+        username: String::from("usernametest"),
+        ..user1
+    };
+    println!("user2: {}, {}, {}, {}", user2.username, user2.email, user2.active, user2.sign_in_count);
 }
