35fd77129c4762233b396c28dd27728623c735a5
Riccardo Persiani <r.persiani92@gmail.com>
2019-12-23 17:01:10 +0100

    chore(rectangle-area)

---
+++ rectangle_are/target/debug/deps/rectangle_are-18107f86e03781e1.d
@@ -0,0 +1,5 @@
+/Users/riccardopersiani/workspace/rust-playground/rectangle_are/target/debug/deps/rectangle_are-18107f86e03781e1: src/main.rs
+
+/Users/riccardopersiani/workspace/rust-playground/rectangle_are/target/debug/deps/rectangle_are-18107f86e03781e1.d: src/main.rs
+
+src/main.rs:
---
+++ rectangle_are/target/debug/rectangle_are.d
@@ -0,0 +1,1 @@
+/Users/riccardopersiani/workspace/rust-playground/rectangle_are/target/debug/rectangle_are: /Users/riccardopersiani/workspace/rust-playground/rectangle_are/src/main.rs
