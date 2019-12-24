26f580e24cd03e26bee0af1d1a692522b899970a
Riccardo Persiani <r.persiani92@gmail.com>
2019-12-24 12:06:21 +0100

    chore(enums)

---
+++ enums/target/debug/deps/nums-c5ebf642a0d609fb.d
@@ -0,0 +1,5 @@
+/Users/riccardopersiani/workspace/rust-playground/enums/target/debug/deps/nums-c5ebf642a0d609fb: src/main.rs
+
+/Users/riccardopersiani/workspace/rust-playground/enums/target/debug/deps/nums-c5ebf642a0d609fb.d: src/main.rs
+
+src/main.rs:
---
+++ enums/target/debug/nums.d
@@ -0,0 +1,1 @@
+/Users/riccardopersiani/workspace/rust-playground/enums/target/debug/nums: /Users/riccardopersiani/workspace/rust-playground/enums/src/main.rs
