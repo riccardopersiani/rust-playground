8a8193c251ec4df14c6e9bfc197858dda5fd5ebf
Riccardo Persiani <r.persiani92@gmail.com>
2019-12-30 15:44:13 +0100

    feat(guessing-game): remove target

--- guessing_game/target/debug/deps/guessing_game-ac7f8808b34a0e37.d
+++
@@ -1,5 +0,0 @@
-/Users/riccardopersiani/workspace/rust-playground/guessing_game/target/debug/deps/guessing_game-ac7f8808b34a0e37: src/main.rs
-
-/Users/riccardopersiani/workspace/rust-playground/guessing_game/target/debug/deps/guessing_game-ac7f8808b34a0e37.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/riccardopersiani/workspace/rust-playground/guessing_game/target/debug/guessing_game.dSYM: /Users/riccardopersiani/workspace/rust-playground/guessing_game/src/main.rs
